83a09c2cf2991455b8220f5e3836df86010ec745
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-04-30 06:54:44 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+python_ces_quoi = input("Sais quoi python pour toi ? : ")
+
+if python_ces_quoi == "Le meileur":
+  print(python_ces_quoi, "Alors veux tu apprendre python maitenant que tu en connais les capacité ?")
+else:
+    print(python_ces_quoi, "Bon bah Bonne journée passe ton chemin :)")
