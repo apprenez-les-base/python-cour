f7b5dbd61d102613a8ce5c6caeac07d12dc8b34b
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-04-30 06:55:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-python_ces_quoi = input("Sais quoi python pour toi ? : ")
+import os
+
+python_ces_quoi = os.getenv("QUESTION")
+
+if python_ces_quoi is None:
+    python_ces_quoi = input("Sais quoi python pour toi ? : ")
 
 if python_ces_quoi == "Le meileur":
-  print(python_ces_quoi, "Alors veux tu apprendre python maitenant que tu en connais les capacité ?")
+    print(python_ces_quoi, "Alors veux tu apprendre python maintenant que tu en connais les capacités ?")
 else:
     print(python_ces_quoi, "Bon bah Bonne journée passe ton chemin :)")
